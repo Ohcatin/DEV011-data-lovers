60b84a0a4cc2ad63cea1919773d9d18731c548e3
Ohcatin <catalinajaviera.ordenes@gmail.com>
2023-10-09 14:50:06 -0300

    ultimos cambios, que ya estaban en main pero algo paso en mi visual que volvio a cambios iniciales

--- src/dataFunctions.js
+++ src/dataFunctions.js
@@ -1,28 +1,34 @@
-// estas funciones son de ejemplo
-
-export const example = () => {
-  return 'example';
+export const filterData = (data, filterBy, value) => {
+  const filteredData = data.films.filter((film) => film[filterBy] === value);
+  return filteredData;
 };
 
-export const anotherExample = () => {
-  return [];
-};
-import { renderItems } from './view.js';
-//recibe los datos y los ordena según los parámetros especificados
-export const sortData = (data, sortBy, sortOrder) => {
-  console.log('Antes de ordenar:', data);
-  console.log(sortBy, sortOrder);
+export const sortData = (dataItems, sortBy, sortOrder) => {
+  const copyData = [...dataItems];
   const compare = (a, b) => {
-    if (sortOrder === 'asc') {
-      return a[sortBy] > b[sortBy] ? 1 : -1;
-    } else if (sortOrder === 'desc') {
-      return b[sortBy] > a[sortBy] ? 1 : -1;
+    const sortByA = parseFloat(a[sortBy]);
+    const sortByB = parseFloat(b[sortBy]);
+    if (sortOrder === "asc") {
+      return sortByA > sortByB ? 1 : -1;
+    } else if (sortOrder === "desc") {
+      return sortByB > sortByA ? 1 : -1;
     }
   };
-  data.sort(compare);
-  console.log('Después de ordenar:', data);
+  const sortedData = copyData.sort(compare);
+  return sortedData;
+};
 
-  renderItems(data);
-  return data;
-}
-
+export const peopleData = (film) => {
+  const dataPeople = film.people.map((i) => {
+    return i.name;
+  });
+  return dataPeople;
+};
+export const computeStat = (data) => {
+  const totalRanking = data.reduce(
+    (sum, film) => sum + parseFloat(film.rt_score),
+    0
+  );
+  const promedioRanking = totalRanking / data.length;
+  return promedioRanking.toFixed(2);
+};
